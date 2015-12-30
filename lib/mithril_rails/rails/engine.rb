595126d2236712c4ba59b60301954cf7f376aeab
Matt Powell <fauxparse@gmail.com>
2015-12-31 09:54:17 +1300

    stop engine loading error in Rails 5 beta

--- lib/mithril_rails/rails/engine.rb
+++ lib/mithril_rails/rails/engine.rb
@@ -2,7 +2,7 @@ module MithrilRails
   module Rails
     class Engine < ::Rails::Engine
       initializer "mithril_rails.setup_engine", :group => :all do |app|
-        app.assets.register_engine '.msx', MithrilRails::MSX::Template
+        app.assets.register_engine '.msx', MithrilRails::MSX::Template if app.assets
       end
     end
   end
