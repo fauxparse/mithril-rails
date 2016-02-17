c4b33013c3d3d2ddd4acfc8982c1e52dab2fe7c3
Matt Powell <matt.powell@powershop.co.nz>
2016-02-18 10:58:45 +1300

    fix engine registration

--- lib/mithril_rails/rails/engine.rb
+++ lib/mithril_rails/rails/engine.rb
@@ -2,7 +2,9 @@ module MithrilRails
   module Rails
     class Engine < ::Rails::Engine
       initializer "mithril_rails.setup_engine", :group => :all do |app|
-        app.assets.register_engine '.msx', MithrilRails::MSX::Template
+        ::Rails.application.config.assets.configure do |env|
+          env.register_engine '.msx', MithrilRails::MSX::Template
+        end
       end
     end
   end
