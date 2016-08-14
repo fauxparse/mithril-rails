d27fe3e9b5a2ff4efd4e585f2bc51a9e4772e8b4
Matt Powell <fauxparse@gmail.com>
2016-08-14 22:18:31 +1200

    don't bother with register_engine

--- lib/mithril_rails/rails/engine.rb
+++ lib/mithril_rails/rails/engine.rb
@@ -2,9 +2,9 @@ module MithrilRails
   module Rails
     class Engine < ::Rails::Engine
       initializer "mithril_rails.setup_engine", :group => :all do |app|
-        ::Rails.application.config.assets.configure do |env|
-          env.register_engine '.msx', MithrilRails::MSX::Template
-        end
+        # ::Rails.application.config.assets.configure do |env|
+        #   env.register_engine '.msx', MithrilRails::MSX::Template
+        # end
       end
     end
   end
